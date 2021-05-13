8e7456d7301c5f39d32b016d5e03f117ae42d8ca
easbar <easbar.mail@posteo.net>
2021-05-13 11:58:33 +0200

    Fix example

--- example/standalone-example.js
+++ example/standalone-example.js
@@ -1,6 +1,7 @@
 import {HeightGraph} from "../src/heightgraph"
 
 const map = document.createElement('div');
+document.body.appendChild(map);
 const hg = new HeightGraph(map);
 hg.setData({
     "type": "FeatureCollection",
